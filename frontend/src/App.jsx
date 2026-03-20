fa9213aab8294a6c8f8eddaecceda405a1d7f99b
alejbrata <alejandrobravot3344@gmail.com>
2026-03-20 14:50:05 +0100

    fix: UI force display block for line numbers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -140,7 +140,8 @@ function App() {
                           language="python" 
                           style={vscDarkPlus} 
                           showLineNumbers={true}
-                          wrapLongLines={true}
+                          wrapLines={true}
+                          lineProps={{ style: { display: "block" } }}
                           customStyle={{margin: 0, borderRadius: '6px', fontSize: '0.85rem', fontFamily: "'Fira Code', monospace"}}
                         >
                           {scriptCode ? String(scriptCode).replace(/\r\n/g, '\n') : ''}
